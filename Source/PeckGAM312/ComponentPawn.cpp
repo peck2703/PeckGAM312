54ffd9d33df95a53823ce7bd453be161262f786b
peck2703 <mrpeckjr@gmail.com>
2018-11-17 14:11:28 -0600

    Finished 3-2

--- Source/PeckGAM312/ComponentPawn.cpp
+++ Source/PeckGAM312/ComponentPawn.cpp
@@ -19,7 +19,6 @@ AComponentPawn::AComponentPawn()
 	//Create a sphere component
 	USphereComponent* Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
 	//make the sphere the root component
-
 	RootComponent = Sphere;
 	//Initiate radius size
 	Sphere->InitSphereRadius(50.0f);
@@ -28,7 +27,7 @@ AComponentPawn::AComponentPawn()
 
 	//Create the mesh and then attach it to the root component that we created above
 	UStaticMeshComponent* SphereMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Visual"));
-	SphereMesh->AttachTo(RootComponent);
+	SphereMesh->SetupAttachment(RootComponent);
 
 	//Appears to be finding a mesh to add to the pawn, but do we have to have the starter content availble for it to work?
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
@@ -45,27 +44,38 @@ AComponentPawn::AComponentPawn()
 
 	// Create a particle system that we can activate or deactivate
 	OurParticleSystem = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("MovementParticles"));
+	//Attach the particle system to the Sphere Mesh
 	OurParticleSystem->SetupAttachment(SphereMesh);
+	//Disable to particle system at first until it's toggled
 	OurParticleSystem->bAutoActivate = false;
+	//Set initial location for the particle system relative to the sphere mesh
 	OurParticleSystem->SetRelativeLocation(FVector(-20.0f, 0.0f, 20.0f));
+	//Find the actual asset in the Content folder
 	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleAsset(TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire"));
+	//if the file can be found, then set the template
 	if (ParticleAsset.Succeeded())
 	{
 		OurParticleSystem->SetTemplate(ParticleAsset.Object);
 	}
-
 	// Use a spring arm to give the camera smooth, natural-feeling motion.
 	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachmentArm"));
+	//Attach the spring arm to the root component
 	SpringArm->SetupAttachment(RootComponent);
+	//set the intial rotation of the camera relative to the root component
 	SpringArm->RelativeRotation = FRotator(-45.f, 0.f, 0.f);
+	//Set the length of the spring arm
 	SpringArm->TargetArmLength = 400.0f;
+	//Sets a camera lag to create a blend effect
 	SpringArm->bEnableCameraLag = true;
+	//Set the speed of the lag
 	SpringArm->CameraLagSpeed = 3.0f;
 
 	// Create a camera and attach to our spring arm
-	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("ActualCamera"));
+	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	//Attach the camera to the spring arm component
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
 
+	//Auto possess the object 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 	// Create an instance of our movement component, and tell it to update our root component.
@@ -96,32 +106,29 @@ void AComponentPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	PlayerInputComponent->BindAxis("MoveRight", this, &AComponentPawn::MoveRight);
 	PlayerInputComponent->BindAxis("Turn", this, &AComponentPawn::Turn);
 }
-
 UPawnMovementComponent * AComponentPawn::GetMovementComponent() const
 {
 	return OurMovementComponent;
 }
-
-void AComponentPawn::MoveForward(float AxisValue)
+void AComponentPawn::MoveForward(float value)
 {
 	if (OurMovementComponent && (OurMovementComponent->UpdatedComponent == RootComponent))
 	{
-		OurMovementComponent->AddInputVector(GetActorForwardVector() * AxisValue);
+		OurMovementComponent->AddInputVector(GetActorForwardVector() * value);
 	}
 }
-
-void AComponentPawn::MoveRight(float AxisValue)
+void AComponentPawn::MoveRight(float value)
 {
 	if (OurMovementComponent && (OurMovementComponent->UpdatedComponent == RootComponent))
 	{
-		OurMovementComponent->AddInputVector(GetActorRightVector() * AxisValue);
+		OurMovementComponent->AddInputVector(GetActorRightVector() * value);
 	}
 }
 
-void AComponentPawn::Turn(float AxisValue)
+void AComponentPawn::Turn(float value)
 {
 	FRotator NewRotation = GetActorRotation();
-	NewRotation.Yaw += AxisValue;
+	NewRotation.Yaw = value;
 	SetActorRotation(NewRotation);
 }
 
